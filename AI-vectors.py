65ae8f21a483ae5b6c7ccf0f7cc20d48fc45ffc0
devon harvey <yrn2424@gmail.com>
2024-12-30 23:33:34 -0500

    final functions

--- AI-vectors.py
+++ AI-vectors.py
@@ -9,5 +9,18 @@ import numpy as np
 def vector_addition(vec1, vec2):
     return np.array(vec1) + np.array(vec2)
 
+
+def vector_subtraction(vec1, vec2):
+    return np.array(vec1) - np.array(vec2)
+
 def vector_magnitude(vec):
     return math.sqrt(sum(x**2 for x in vec))
+
+def dot_product(vec1, vec2):
+    return np.dot(vec1, vec2)
+
+def vector_proprtions(vec1, vec2):
+    m1 = vector_magnitude(vec1)
+    m2 = vector_magnitude(vec2)
+    return m1/m2
+    
